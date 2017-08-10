3a777b748ef7289981c51d176fe2eb7653ea3ef7
peter.panchine <peter.panchine@TORRVT-NW71546.na.corp.ipgnetwork.com>
2017-08-10 11:41:44 -0400

    commit#2

---
+++ src/destinationPride/Main.java
@@ -0,0 +1,68 @@
+package destinationPride;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeMethod;
+
+import static org.testng.Assert.assertEquals;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+
+public class Main {
+	WebDriver driver; 
+	     
+  @Test (priority= 0, description= "Correct URl")
+  public void URL() {
+	  
+	  driver.get("https://qa.destinationpride.org/app_qa.php/");
+
+	   driver.manage().window().maximize();
+	   // to maximize window
+	   
+	   assertEquals(driver.getTitle(), "Destination Pride - Data-driven LGBTQ+ scores for every city, every country on earth.");
+	   
+	  /* if (driver.getTitle().contains("Destination Pride - Data-driven LGBTQ+ scores for every city, every country on earth.")){
+				  System.out.println("pass");
+			  }
+			  else {
+				  System.out.println("wrong Url" + driver.getTitle());		
+			  } */
+  }
+  
+  @Test 
+  public void links() {
+	  
+	  
+  }
+  
+  
+  @BeforeMethod
+  public void beforeMethod() {
+	 System.setProperty("webdriver.chrome.driver", "C:\\Users\\peter.panchine\\Downloads\\chromedriver_win32\\chromedriver.exe");
+	 driver = new ChromeDriver(); 
+	 
+	   // to be able to use Chrome browser
+
+	      
+	
+	
+
+				  
+		   
+		 
+	 
+	  
+	  
+	  
+
+  }
+
+  @AfterMethod
+  public void afterMethod() {
+	  
+	  driver.quit();
+
+  }
+
+}
